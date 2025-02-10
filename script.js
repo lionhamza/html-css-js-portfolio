c7d86a2305371f221bf414bc02997189c747d1d2
Hamza Madi <166062319+lionhamza@users.noreply.github.com>
2025-02-10 10:54:07 +0200

    Agile third iterative cycle

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menuLinks = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menuLinks.classiList.toggleMenu("Open");
+    icon.classList.toggleMenu("Open")
+}
